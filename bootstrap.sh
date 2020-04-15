7657ccc45b7eba55a7488f73bcbde20b4c6eb346
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-04-15 21:09:56 +0800

    Add basic files

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# download latest release and unzip it
+curl -LO https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz
+tar xvf subconverter_linux64.tar.gz
+# change extension to make now recognize it
+mv -f subconverter/subconverter subconverter/subconverter.exe
+# overwrite preference settings
+cp -f pref.ini subconverter/
+
