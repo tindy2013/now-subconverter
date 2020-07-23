351185bfad11f8019d56e13eadf295cf18ddcc3e
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-07-23 18:25:13 +0800

    Update example

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 # download latest release and unzip it
 curl -LO https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz
 tar xvf subconverter_linux64.tar.gz
+rm subconverter_linux64.tar.gz
 # change extension to make now recognize it
 mv -f subconverter/subconverter subconverter/subconverter.exe
 # overwrite preference settings
-cp -f pref.ini subconverter/
-
+cp -R profiles/* subconverter/
